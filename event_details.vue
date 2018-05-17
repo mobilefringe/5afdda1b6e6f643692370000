38d66c28c3a8943a75157a1c1743a1244f12d279
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-17 17:17:20 -0400

    Casas Adobes Plaza 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -87,7 +87,7 @@
                 currentEvent : function (){
                     console.log(this.currentEvent)
                     if(this.currentEvent != null) {
-                        if (this.currentEvent.promotionable_type === "Store"){
+                        if (this.currentEvent.eventable_type === "Store"){
                             if  (_.includes(this.currentEvent.promo_image_url_abs, 'missing')) {
                                 this.currentEvent.image_url = this.currentEvent.store.store_front_url_abs; 
                             }
