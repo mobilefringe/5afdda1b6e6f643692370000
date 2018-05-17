f991270e15f8ab9b4848710f561be3a1876238a3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-17 16:54:20 -0400

    Casas Adobes Plaza 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -63,6 +63,7 @@
             computed: {
                 ...Vuex.mapGetters([
                     'property',
+                    'findRepoByName',
                     'processedStores'
                 ]),
                 allStores() {
