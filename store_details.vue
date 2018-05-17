eed9e99fc1d5a00f329ba12019e041389ad890c9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-17 16:35:43 -0400

    Casas Adobes Plaza 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -200,6 +200,7 @@
                 ...Vuex.mapGetters([
                     'property',
                     'timezone',
+                    'findRepoByName',
                     'stores',
                     'findStoreBySlug',
                     'findCategoryById',
@@ -222,7 +223,7 @@
             methods: {
                 loadData: async function () {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData","jobs")]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData","jobs")]);
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
