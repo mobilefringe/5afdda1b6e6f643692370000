535904207cb4f82ae3167fc528e721951b3a3747
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-05-30 17:16:03 -0400

    Casas Adobes Plaza 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -11,7 +11,7 @@
                 <div class="main_container mobile_padding margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
-                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5afdda1b6e6f643692370000/image/jpeg/1526584268000/side_banner8.jpg" alt="" />    
+                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5afdda1b6e6f643692370000/image/jpeg/1527708236000/Casas Adobes - Aerial 03.jpg" alt="" />    
                         </div>
                         <div class="details_col_9">
                             <div class="page_body" v-html="currentPage.body"></div>
@@ -24,59 +24,5 @@
 </template>
 
 <script>
-    define(["Vue", "vuex"], function (Vue, Vuex) {
-        return Vue.component("page-details-component", {
-            template: template, // the variable template will be injected,
-            props: ['id'],
-            data: function data() {
-                return {
-                    dataLoaded: false,
-                    pageBanner: "",
-                    currentPage: null
-                }
-            },
-            created(){
-                this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Inside Page Banner');
-                    if(temp_repo) {
-                        this.pageBanner = temp_repo.images[0];
-                    }
-
-                    this.updateCurrentPage(this.id);
-                    this.dataLoaded = true;
-                });
-            },
-            watch: {
-                $route: function () {
-                    this.updateCurrentPage(this.$route.params.id);
-                }
-            },
-            computed: {
-                ...Vuex.mapGetters([
-                    'property',
-                    'findRepoByName'
-                ])
-            },
-            methods: {
-                loadData: async function () {
-                    try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
-                        return results;
-                    } catch (e) {
-                        console.log("Error loading data: " + e.message);
-                    }
-                },
-                updateCurrentPage(id) {
-                    var _this = this;
-                    this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + this.id + ".json" }).then(function (response) {
-                        _this.currentPage = response.data;
-                        _this.dataLoaded = true;
-                    }, function (error) {
-                        console.error( "Could not retrieve data from server. Please check internet connection and try again.");
-                        _this.$router.replace({ name: '404' });
-                    });
-                }
-            }
-        });
-    });
+var _extends=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var a=arguments[t];for(var r in a)Object.prototype.hasOwnProperty.call(a,r)&&(e[r]=a[r])}return e};define(["Vue","vuex"],function(e,t){return e.component("page-details-component",{template:template,props:["id"],data:function(){return{dataLoaded:!1,pageBanner:"",currentPage:null}},created:function(){var e=this;this.loadData().then(function(){var t=e.findRepoByName("Inside Page Banner");t&&(e.pageBanner=t.images[0]),e.updateCurrentPage(e.id),e.dataLoaded=!0})},watch:{$route:function(){this.updateCurrentPage(this.$route.params.id)}},computed:_extends({},t.mapGetters(["property","findRepoByName"])),methods:{loadData:function(){var e;return regeneratorRuntime.async(function(t){for(;;)switch(t.prev=t.next){case 0:return t.prev=0,t.next=3,regeneratorRuntime.awrap(Promise.all([this.$store.dispatch("getData","repos")]));case 3:return e=t.sent,t.abrupt("return",e);case 7:t.prev=7,t.t0=t["catch"](0),console.log("Error loading data: "+t.t0.message);case 10:case"end":return t.stop()}},null,this,[[0,7]])},updateCurrentPage:function(){var e=this;this.$store.dispatch("LOAD_PAGE_DATA",{url:this.property.mm_host+"/pages/"+this.id+".json"}).then(function(t){e.currentPage=t.data,e.dataLoaded=!0},function(){console.error("Could not retrieve data from server. Please check internet connection and try again."),e.$router.replace({name:"404"})})}}})});
 </script>
