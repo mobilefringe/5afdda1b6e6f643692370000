27a923536e460b8e532414d99ba910151a5a61b6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-31 12:48:15 -0400

    Casas Adobes Plaza 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -86,6 +86,13 @@
                 ])
             },
             methods: {
+                loadData: async function () {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
 				isMultiDay(currentPromo) {
 					var timezone = this.timezone
 					var start_date = moment(currentPromo.start_date).tz(timezone).format("MM-DD-YYYY")
