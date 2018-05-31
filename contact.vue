84281bee15826128c40713a9dd2dc1329adb1b99
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-31 12:05:43 -0400

    Casas Adobes Plaza 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -46,7 +46,6 @@
                                         </button>
                                     </div>
                                 </form>
-                                <br/>
                                 <div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">
                                     <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
                                     <span class="sr-only">Success</span>
