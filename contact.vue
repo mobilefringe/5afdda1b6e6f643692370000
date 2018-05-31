7e27573d5a4263e8a7a8fa017d1a659eb24b76c6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-31 12:25:39 -0400

    Casas Adobes Plaza 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -41,7 +41,7 @@
                                         <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
                                     </div>
                                     <div class="col-xs-12">
-                                        <button class="submit fill_btn" type="submit" :disabled="formSuccess">
+                                        <button class="animated_btn" type="submit" :disabled="formSuccess">
                                             Send <i class="fa fa-angle-right" aria-hidden="true"></i>
                                         </button>
                                     </div>
