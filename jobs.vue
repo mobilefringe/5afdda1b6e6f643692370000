cd1155c25c50ea9e9120474a58e53598047f0e1b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-31 13:24:16 -0400

    Casas Adobes Plaza 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -66,5 +66,75 @@
 </template>
 
 <script>
-var _extends=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var r in n)Object.prototype.hasOwnProperty.call(n,r)&&(e[r]=n[r])}return e};define(["Vue","vuex","moment","moment-timezone","vue-moment","bootstrap-vue"],function(e,t,n,r,o,a){return e.use(a),e.component("jobs-component",{template:template,data:function(){return{dataLoaded:!1,pageBanner:"",toggleJobs:!1}},created:function(){var e=this;this.loadData().then(function(){var t=e.findRepoByName("Jobs Banner");t&&(e.pageBanner=t.images[0]),e.dataLoaded=!0})},computed:_extends({},t.mapGetters(["property","timezone","findRepoByName","processedJobs"]),{jobList:function s(){console.log(this.processedJobs);var s=this.processedJobs,e=[];_.forEach(s,function(t){var r=n.tz(this.timezone).format(),o=n.tz(t.show_on_web_date,this.timezone).format();r>=o&&(t.description_short=_.truncate(t.description,{length:100,separator:" "}),e.push(t))});var t=_.orderBy(e,function(e){return e.end_date});return t.length>0&&(this.toggleJobs=!0),t}}),methods:{loadData:function(){var e;return regeneratorRuntime.async(function(t){for(;;)switch(t.prev=t.next){case 0:return t.prev=0,t.next=3,regeneratorRuntime.awrap(Promise.all([this.$store.dispatch("getData","repos"),this.$store.dispatch("getData","jobs")]));case 3:return e=t.sent,t.abrupt("return",e);case 7:t.prev=7,t.t0=t["catch"](0),console.log("Error loading data: "+t.t0.message);case 10:case"end":return t.stop()}},null,this,[[0,7]])},isMultiDay:function(e){var t=this.timezone,r=n(e.start_date).tz(t).format("MM-DD-YYYY"),o=n(e.end_date).tz(t).format("MM-DD-YYYY");return r===o?!1:!0}}})});
+define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue"], function (Vue, Vuex, moment, tz, VueMoment, BootstrapVue) {
+        Vue.use(BootstrapVue);
+        return Vue.component("jobs-component", {
+            template: template, // the variable template will be injected
+            data: function () {
+                return {
+                    dataLoaded: false,
+                    pageBanner: "",
+                    toggleJobs: false,
+                }
+            },
+            created() {
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Jobs Banner');
+                    if(temp_repo) {
+                        this.pageBanner = temp_repo.images[0];
+                    }
+                    
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName',
+                    'processedJobs'
+                ]),
+                jobList: function jobs() {
+                    console.log(this.processedJobs)
+                    var jobs = this.processedJobs;
+                    var showJobs = [];
+                    _.forEach(jobs, function (value, key) {
+                        var today = moment.tz(this.timezone).format();
+                        var showOnWebDate = moment.tz(value.show_on_web_date, this.timezone).format();
+                        if (today >= showOnWebDate) {
+
+                            value.description_short = _.truncate(value.description, { 'length': 100, 'separator': ' ' });
+                            
+                            showJobs.push(value);
+                        }
+                    });
+                    var sortedJobs = _.orderBy(showJobs, function (o) { return o.end_date });
+                    if (sortedJobs.length > 0) {
+                        this.toggleJobs = true;
+                    }
+                    return sortedJobs
+                },
+            },
+            methods: {
+                loadData: async function () {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch("getData", "jobs")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                isMultiDay(promo) {
+                    var timezone = this.timezone
+                    var start_date = moment(promo.start_date).tz(timezone).format("MM-DD-YYYY")
+                    var end_date = moment(promo.end_date).tz(timezone).format("MM-DD-YYYY")
+                    if (start_date === end_date) {
+                        return false
+                    } else {
+                        return true
+                    }
+                }
+            }
+        });
+    });
 </script>
