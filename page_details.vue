ddaad2d285f02a90eaafd27f6a681b1d61528a86
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-31 12:48:22 -0400

    Casas Adobes Plaza 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -56,7 +56,7 @@
             methods: {
                 loadData: async function () {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch("getData", "events"), this.$store.dispatch("getData","promotions")]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
