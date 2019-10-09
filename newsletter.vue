b1806f683942905be21b56eb88c72b32fd88cd67
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-09 11:33:24 -0400

    Casas Adobes Plaza 1.1  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -27,16 +27,16 @@
                                 <div class="row">
                                     <div class="col-sm-8" >
                                         <label for="cm-name">Name</label>
-                                        <input v-model="form_data.name" required class="margin_20 form-control" name="cm-name" type="text" placeholder="Name">
+                                        <input v-model="form_data.name" required class="margin_20 form-control" id="cm-name" type="text" placeholder="Name">
                                     </div>
                                     <div class="col-sm-8">
                                         <label for="cm-vltuui-vltuui">Email</label>
-                                        <input v-model="form_data.email" required class="margin_20 form-control" name="cm-vltuui-vltuui" type="email" placeholder="Email" id="newsletter_email">
+                                        <input v-model="form_data.email" required class="margin_20 form-control" id="cm-vltuui-vltuui" type="email" placeholder="Email" id="newsletter_email">
                                     </div>
                                     <div class="col-sm-8">
                                         <div style="margin-left: 20px">
-                                            <label class="checkbox">
-                                                <input name="agree_newsletter" required  type="checkbox">
+                                            <label class="checkbox" for="agree">
+                                                <input name="agree_newsletter" required  type="checkbox" id="agree">
                                                     I agree to receive communications from {{ property.name }}.
                                             </label>
                                         </div>
