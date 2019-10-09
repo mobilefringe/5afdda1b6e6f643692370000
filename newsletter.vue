fc3c8b0e53697d869846d406fb7f24b0039baafa
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-09 11:34:41 -0400

    Casas Adobes Plaza 1.1  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -30,8 +30,8 @@
                                         <input v-model="form_data.name" required class="margin_20 form-control" id="cm-name" type="text" placeholder="Name">
                                     </div>
                                     <div class="col-sm-8">
-                                        <label for="cm-vltuui-vltuui">Email</label>
-                                        <input v-model="form_data.email" required class="margin_20 form-control" id="cm-vltuui-vltuui" type="email" placeholder="Email" id="newsletter_email">
+                                        <label for="newsletter_email">Email</label>
+                                        <input v-model="form_data.email" required class="margin_20 form-control" name="cm-vltuui-vltuui" type="email" placeholder="Email" id="newsletter_email">
                                     </div>
                                     <div class="col-sm-8">
                                         <div style="margin-left: 20px">
