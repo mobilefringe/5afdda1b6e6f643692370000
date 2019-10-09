177003d5e9ecc77a4e1ceaf37df9e695584ef467
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-09 11:59:15 -0400

    Casas Adobes Plaza 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -32,7 +32,8 @@
                     <nav id="primary_nav">
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
-						        <router-link :to="item.href">{{ item.name }}</router-link>
+						        <span v-if="!item.href">{{item.name}}</span>
+						        <router-link v-else :to="item.href">{{ item.name }}</router-link>
 						        <ul v-if="item.sub_menu">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <router-link :to="sub_menu.href">{{ sub_menu.name }}</router-link>
