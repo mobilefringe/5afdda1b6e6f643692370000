34163e0bd3971d66a90580b7e11de0de3093e03c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-25 22:49:08 -0500

    Casas Adobes Plaza 1.1  /footer.vue

--- footer.vue
+++ footer.vue
@@ -3,7 +3,7 @@
         <section class="footer_menu main_container">
             <div class="row">
                 <div class="col-xs-12 col-sm-6 col-md-4 footer_hours">
-                    <iframe role="img" :aria-label="'Google Map showing surrounding area of ' + property.name" title="Map" width="100%" height="200px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'https://maps.google.nl/maps?q=' + getPropertyAddressMap + '&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear=' + getPropertyAddress + '&amp;z=16&amp;output=embed'"></iframe>
+                    <iframe role="img" :aria-label="'Google Map showing surrounding area of ' + property.name" title="Map" width="100%" height="200px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" :src="propertyAddress()"></iframe>
                     <div class="clearfix"></div>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 footer_newsletter">
@@ -100,6 +100,12 @@
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
+                },
+                propertyAddress() {
+                    var address = this.property.name + "+" + this.property.address1 + "+" + this.property.city + "+" + this.property.province_state + "+" + this.property.country + this.property.postal_code
+                    var key ="AIzaSyCukCjH3fsuDYBdI44hZKL43m60jEToJjY"
+                    var src = "https://www.google.com/maps/embed/v1/place?q=" + address + "&key="+ key
+                    return src
                 },
                 newsletterRoute() {
                     this.show_menu = false;
