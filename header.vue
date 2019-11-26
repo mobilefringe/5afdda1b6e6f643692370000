46f7e75d7d17cd7eca4af3e090b2d0fdd1c7a9f0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-25 22:43:58 -0500

    Casas Adobes Plaza 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -28,7 +28,7 @@
                             </span>
                         </div>
                     </div>
-                    <nav id="primary_nav">
+                    <nav id="primary_nav" role="navigation" aria-label="Main">
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
 								<router-link v-if="!item.sub_menu" :to="item.href">{{ item.name }}</router-link>
@@ -43,20 +43,20 @@
 					</nav>
 					<div class="nav_container visible_phone">
 					    <transition name="custom-classes-transition" enter-active-class="animated slideInDown" leave-active-class="animated slideOutUp">
-    					    <nav id="mobile_nav" v-show="showMenu" class="">
+    					    <nav id="mobile_nav" v-show="showMenu" class="" role="navigation" aria-label="Mobile">
     					        <ul>
     					            <li v-for="(item,key) in menu_items" class="menu_item">
     							        <router-link :to="item.href" v-if="item.sub_menu == undefined">{{$t(item.name)}}</router-link>
     							        <div v-else>
     							            <b-card no-body class="mb-1">
-                                                <b-card-header header-tag="header" class="p-1" role="tab">
+                                                <b-card-header header-tag="header" class="p-1">
                                                     <b-btn block @click="item.show_sub_menu = !item.show_sub_menu" :class="item.show_sub_menu ? 'collapsed' : null" :aria-controls="$t(item.name)" :aria-expanded="item.show_sub_menu ? 'true' : 'false'">
                                                         {{$t(item.name)}}
                                                         <i v-if="item.show_sub_menu"  class="fa fa-minus"></i>
                                                         <i v-else  class="fa fa-plus"></i>
                                                     </b-btn>
                                                 </b-card-header>
-                                                <b-collapse v-model="item.show_sub_menu" :id="$t(item.name)" :visible="item.show_sub_menu" :accordion="$t(item.name)" role="tabpanel" class="accordion_body">
+                                                <b-collapse v-model="item.show_sub_menu" :id="$t(item.name)" :visible="item.show_sub_menu" :accordion="$t(item.name)" class="accordion_body">
                                                     <b-card-body v-for="sub_menu in item.sub_menu">
                                                         <p class="card-text">
                                                             <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
