7486e62ee49500a39ebdc6f36a8a94c2440ab48d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-27 15:04:06 -0500

    Casas Adobes Plaza 1.1  /location.vue

--- location.vue
+++ location.vue
@@ -27,7 +27,7 @@
                             
                         </div>
                         <div class="details_col_9">
-                            <iframe aria-hidden="true" width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'https://maps.google.nl/maps?q=' + getPropertyAddress + '&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear=' + getPropertyAddress + '&amp;z=16&amp;output=embed'"></iframe>
+                            <iframe role="presentation" aria-hidden="true" width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'https://maps.google.nl/maps?q=' + getPropertyAddress + '&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear=' + getPropertyAddress + '&amp;z=16&amp;output=embed'"></iframe>
                             <hr>
                             <div class="page_content" v-if="currentPage" v-html="currentPage.body"></div>
                         </div>
