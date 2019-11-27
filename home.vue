706f8cf87580a0c7aebc67440bd2e6149760ccef
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-27 15:35:10 -0500

    Casas Adobes Plaza 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -1,5 +1,5 @@
 <template>
-    <main><!-- without an outer container div this component template will not render -->
+    <div><!-- without an outer container div this component template will not render -->
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
@@ -27,7 +27,7 @@
                 </div>
             </div>
         </transition>
-    </main>
+    </div>
 </template>
 <script>
     define(["Vue", "vuex", "vue-meta", "vue!vue-slick"], function (Vue, Vuex, Meta, slick) {
